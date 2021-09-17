6b758fc0cdea770261a020bc45e7b1a5672e65bd
Ankit Kumar <ankit.tec40@gmail.com>
2021-09-17 19:00:09 +0530

    1st

---
+++ GetWeather/app/src/main/java/com/tecwec/getweather/MainActivity.java
@@ -0,0 +1,76 @@
+package com.tecwec.getweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RelativeLayout homeRl;
+    private ProgressBar loadingPB;
+    private TextView cityNameTV,temperatureTV,conditionTV;
+    private RecyclerView weatherRV;
+    private TextInputEditText cityEdt;
+    private ImageView backIV,iconIV,searchIV;
+
+    private ArrayList<WeatherRVModal> weatherRVModalArrayList;
+    private WeatherRVAdapter weatherRVAdapter;
+
+    private LocationManager locationManager;
+    private int PERMISSION_CODE=1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        setContentView(R.layout.activity_main);
+
+        homeRl=findViewById(R.id.idRLHome);
+        loadingPB=findViewById(R.id.idPBLoading);
+        cityNameTV=findViewById(R.id.idTVCityName);
+        temperatureTV=findViewById(R.id.idTVTemperature);
+        conditionTV=findViewById(R.id.idTVCondition);
+        weatherRV=findViewById(R.id.idRVWeather);
+        cityEdt=findViewById(R.id.idEdtCity);
+        backIV=findViewById(R.id.idIVBack);
+        iconIV=findViewById(R.id.idIVIcon);
+        searchIV=findViewById(R.id.idIVSearch);
+
+        weatherRVModalArrayList=new ArrayList<>();
+        weatherRVAdapter=new WeatherRVAdapter(this.weatherRVModalArrayList);
+        weatherRV.setAdapter(weatherRVAdapter);
+
+        locationManager=(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},PERMISSION_CODE);
+        }
+
+        Location location=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        ////////////////////////////////////////////////////////////////
+
+    }
+
+    private void getWeatherInfo(String cityName){
+        String url="http://api.weatherapi.com/v1/current.json?key=15a701cbb84e403285982054210709&q="+ cityName +"&aqi=no";
+
+    }
+
+}
